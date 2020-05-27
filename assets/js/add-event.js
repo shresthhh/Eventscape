e9dcd1c3770c0d0104d1fdb36470099adc389ee9
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2020-05-28 02:22:50 +0530

    Fixed navbar issue on add event page

---
+++ assets/js/add-event.js
@@ -0,0 +1,19 @@
+$(".navigation__button").on('click',function(){
+    //var str=0;
+    $("nav").toggleClass("navigation__nav--active");
+    $(".navigation__background").toggleClass("navigation__background--active");
+    $(".navigation__icon").toggleClass("navigation__icon--active");
+    // document.styleSheets[0].addRule(".navigation__icon::before", "top: 0 !important; transform: rotate(135deg);", 4);
+    // document.styleSheets[0].addRule(".navigation__icon::after", "top: 0 !important; transform: rotate(-135deg);", 4);  
+});
+const menuBtn = document.querySelector('.navigation__button');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
